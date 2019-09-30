b912fdc6e900aae3b5f25f726abf9d05a8132664
Nyki <nykilundgren127@gmail.com>
2019-09-30 01:19:52 -0400

    fine tuned project.

--- js/script.js
+++ js/script.js
@@ -1,40 +1,42 @@
 
-function output(){
+ 
+document.getElementById("tempFahr").value = "";
+
+function output(){  
   var eventType=document.querySelector("select");
-  var select.addEventListener("change",output);
-  var choice = select.value;
-  var tempFahr;
-  var x,y;
-  x=document.getElementById("tempF");
-  y=x.elements["tempFahr"];
+  var select=addEventListener("change",output);
+var choice=clothes.value;
+var tempFahr=document.getElementById("tempFahr").value;
+
+  
   if (choice==="casual" && tempFahr <54){
-    var result= tempFahr.concat(" is cold, and your event is ",choice,", you should wear something warm and comfy.");
+    var result= tempFahr.concat(" is cold, and your event is casual, you should wear something warm and comfy.");
     console.log(result);
-} else if (choice==="casual" &&  tempFahr>=54){
-    var result=tempFahr.concat(" is chilly, and your event is ",choice,", you should wear something comfy with a light jacket.");
+} else if (choice==="casual" && tempFahr >=54 && tempFahr<=70){
+    var result=tempFahr.concat(" is chilly, and your event is casual, you should wear something comfy with a light jacket.");
     console.log(result);
 } else if(choice==="casual" && tempFahr>70){
-var result=tempFahr.concat(" is warm, and your event is ",choice,", you should wear something comfy with a light jacket.");
+var result=tempFahr.concat(" is warm, and your event is casual, you should wear something comfy without a jacket.");
     console.log(result);
 } else if (choice==="semiFormal" && tempFahr<54){
-var result=tempFahr.concat(" is cold, and your event is ",choice,", you should wear a polo with a coat.");
+var result=tempFahr.concat(" is cold, and your event is semi formal, you should wear a polo with a coat.");
     console.log(result);
-} else if (choice==="semiFormal" && tempFahr>=54){
-var result=tempFahr.concat(" is chilly, and your event is ",choice,", you should wear a polo with a light jacket.");
+} else if (choice==="semiFormal" && tempFahr>=54 && tempFahr<=70){
+var result=tempFahr.concat(" is chilly, and your event is semil formal, you should wear a polo with a light jacket.");
     console.log(result);
 } else if (choice==="semiFormal" && tempFahr>70){
-var result=tempFahr.concat(" is warm, and your event is ",choice,", you should wear a polo with no jacket.");
+var result=tempFahr.concat(" is warm, and your event is semi formal, you should wear a polo with no jacket.");
     console.log(result);
 } else if (choice==="formal" && tempFahr<54){
 var result=tempFahr.concat(" is cold, and your event is ",choice,", you should wear a suit with a coat.");
     console.log(result);
-} else if (choice==="formal" && tempFahr>=54){
+} else if (choice==="formal" && tempFahr>=54 && tempFahr<=70){
 var result=tempFahr.concat(" is chilly, and your event is ",choice,", you should wear a suit with a jacket.");
     console.log(result);
 } else if (choice==="formal" && tempFahr>70){
 var result=tempFahr.concat(" is warm, and your event is ",choice,", you should wear a polo with no jacket.");
     console.log(result);
 } else{
-console.log()
+console.log();
   }
 }
