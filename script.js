f1f17d1067ee7eeaafaa50f165724571e8f71315
Nyki <nykilundgren127@gmail.com>
2019-09-30 01:00:06 -0400

    fixed buggs, runs code smoothly and prints out result.

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-var eventType=document.querySelector("select");
-var select.addEventListener("change",output());
 
+ 
+document.getElementById("tempFahr").value = "";
 
-function output(){
+function output(){  
+  var eventType=document.querySelector("select");
+  var select=addEventListener("change",output);
+var choice=clothes.value;
+var tempFahr=document.getElementById("tempFahr").value;
 
-  var choice = select.value;
-  var tempFahr="0";
+  
   if (choice==="casual" && tempFahr <54){
     var result= tempFahr.concat(" is cold, and your event is ",choice,", you should wear something warm and comfy.");
     console.log(result);
-} else if (choice==="casual" &&  tempFahr>=54){
+} else if (choice==="casual" && tempFahr >=54){
     var result=tempFahr.concat(" is chilly, and your event is ",choice,", you should wear something comfy with a light jacket.");
     console.log(result);
-} else if(choice==="casual" && tempFahr>70){
+} else if(choice==="casual" && tempFahr >70){
 var result=tempFahr.concat(" is warm, and your event is ",choice,", you should wear something comfy with a light jacket.");
     console.log(result);
 } else if (choice==="semiFormal" && tempFahr<54){
@@ -34,6 +37,6 @@ var result=tempFahr.concat(" is chilly, and your event is ",choice,", you should
 var result=tempFahr.concat(" is warm, and your event is ",choice,", you should wear a polo with no jacket.");
     console.log(result);
 } else{
-console.log()
-}
+console.log();
+  }
 }
